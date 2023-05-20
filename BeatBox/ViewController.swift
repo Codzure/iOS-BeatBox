c7f58c5ce5e277a7b5f9e23fb0c9c53d8a152a58
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2023-05-20 18:22:06 +0300

    first commit

--- BeatBox/ViewController.swift
+++ BeatBox/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -13,7 +15,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        playVideo()
+    }
 
-
+    
+    private func playVideo(){
+        guard let path = Bundle.main.path(forResource: "Duffel Bag", ofType: "mp4") else {
+            debugPrint("Duffel Bag.m4v not found")
+            return
+        }
+        
+        let player = AVPlayer(url:URL(fileURLWithPath: path))
+        let playController = AVPlayerViewController()
+        
+        playController.player = player
+        present(playController, animated: true){
+            player.play()
+        }
+    }
+    
+    
+    private func findVideo(){
+        guard let path = Bundle.main.path(forResource: "Duffel Bag", ofType: "m4v") else {
+            debugPrint("video.m4v not found")
+            return
+        }
+        
+    }
+    
 }
 
