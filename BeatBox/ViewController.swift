a2526afb13a516e33ea3e6879efd2a17c0de46c5
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2023-05-20 10:11:27 +0300

    Initial Commit

---
+++ BeatBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeatBox
+//
+//  Created by Leonard Mutugi on 20/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
